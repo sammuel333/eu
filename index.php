cccf0d8af95004b98d6045b2b23495212d7b9b66
sammuel333 <sammuellazaro99@gmail.com>
2025-08-18 17:21:11 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,18 +1,26 @@
 <?php
-$nome = "sammuel";
-$idade = "16 anos";
-echo "<center><h2>Ola professor</center></h2>";
-echo "<center><h4>meu nome é $nome e tenho $idade</center></h4>";
+$arr = array();
 echo "<hr>";
-$frutas = ["maçã", "banana", "laranja", "uva", "melancia",];
-foreach ($frutas as $fruta) {
-    echo $fruta. "<br>";
-}
+echo "<h2>exercicio 1</h2>";
+$frutas = array("maçã","melão","melancia");
+array_unshift($frutas, "abacaxi");
+print_r($frutas);
 echo "<hr>";
-$numero = 19;
-if ($numero%2 == 0) {
-    echo "<center><h2>par</center></h2>";
-} else {
-    echo "<center><h2>impar</center></h2>";
-}
+echo "<h2>exercicio 2</h2>";
+$cores = array("amarelo","azul","verde","vermelho");
+print_r($cores);
+echo "<hr>";
+echo "<h2>exercicio 3</h2>";
+$animais = array("vaca","boi","coelho","macaco","esquilo","elefante","cachorro");
+array_push($animais,"jumento");
+print_r($animais);
+echo "<hr>";
+echo "<h2>exercicio 4</h2>";
+$dias = array("1-segunda","2-terça","3-quarta","4-quinta","5-sexta","6-sabado","7-domingo");
+echo count($dias);
+print_r($dias);
+echo "<hr>";
+echo "<h2>exercicio 5</h2>";
+$numeros = array("1","2","3","4","5");
+
 ?>
